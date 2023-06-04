d68b9af7e2807c1b8262ae1e96d0a8b120f93b1b
Queenbot524 <135263523+Queenbot524@users.noreply.github.com>
2023-06-05 02:13:18 +0530

    Update animez.py

--- MukeshRobot/modules/animez.py
+++ MukeshRobot/modules/animez.py
@@ -554,7 +554,7 @@ __help__ = """
  ❍ /kayo <ᴀɴɪᴍᴇ>*:* sᴇᴀʀᴄʜ ᴀɴ ᴀɴɪᴍᴇ ᴏɴ ᴀɴɪᴍᴇᴋᴀʏᴏ.ᴄᴏᴍ
  ❍ /airing <ᴀɴɪᴍᴇ>*:* ʀᴇᴛᴜʀɴs ᴀɴɪᴍᴇ ᴀɪʀɪɴɢ ɪɴғᴏ.
 
-☆............𝙱𝚈 » [𝚅𝙸𝙿 𝙱𝙾𝚈](https://t.me/the_vip_boy)............☆
+☆............𝙱𝚈 » [𝗘𝗕𝗗 𝗤𝗡](https://t.me/EBD_QN)............☆
 """
 
 ANIME_HANDLER = DisableAbleCommandHandler("anime", anime)
